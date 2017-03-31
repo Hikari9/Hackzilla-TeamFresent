dd10c775d06fa48b5d2394443d3e850ccb04c13a
Rico Tiongson <thericotiongson@gmail.com>
2017-03-31 13:06:50 +0800

    Added script to get ROI from face detector

--- app/app/src/main/java/com/fresent/fresent/ai/FaceDetection.java
+++ app/app/src/main/java/com/fresent/fresent/ai/FaceDetection.java
@@ -2,6 +2,8 @@ package com.fresent.fresent.ai;
 
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.media.Image;
 import android.util.Log;
 
 import com.fresent.fresent.R;
@@ -9,6 +11,13 @@ import com.fresent.fresent.R;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 
 import java.io.File;
@@ -22,6 +31,34 @@ public class FaceDetection extends ContextWrapper {
     private static final String TAG = "FACE_DETECTION";
     private float relativeFaceSize = 0.2f; // tweak this
     private int absoluteFaceSize = 0;
+
+    public double getScaleFactor() {
+        return scaleFactor;
+    }
+
+    public void setScaleFactor(double scaleFactor) {
+        this.scaleFactor = scaleFactor;
+    }
+
+    public int getMinNeighbors() {
+        return minNeighbors;
+    }
+
+    public void setMinNeighbors(int minNeighbors) {
+        this.minNeighbors = minNeighbors;
+    }
+
+    public int getFlags() {
+        return flags;
+    }
+
+    public void setFlags(int flags) {
+        this.flags = flags;
+    }
+
+    private double scaleFactor = 1.1;
+    private int minNeighbors = 2;
+    private int flags = 2;
 
     public CascadeClassifier getViolaJonesClassifier() {
         return violaJonesClassifier;
@@ -42,6 +79,10 @@ public class FaceDetection extends ContextWrapper {
         return absoluteFaceSize;
     }
 
+    public Size getMinSize() {
+        return new Size(absoluteFaceSize, absoluteFaceSize);
+    }
+
     public void setRelativeFaceSize(float relativeFaceSize) {
         this.relativeFaceSize = relativeFaceSize;
     }
@@ -56,9 +97,43 @@ public class FaceDetection extends ContextWrapper {
             OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, loaderCallback);
         } else {
             Log.d(TAG, "OpenCV library found inside package. Using it.");
-             loaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            loaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
         }
     }
+
+    public Mat convertBitmapToMat(Bitmap image) {
+        Bitmap image32 = image.copy(Bitmap.Config.RGB_565, true);
+        Mat mat = new Mat();
+        Utils.bitmapToMat(image32, mat);
+        return mat;
+    }
+
+    public Rect[] detectFaces(Bitmap image) {
+        return detectFaces(convertBitmapToMat(image));
+    }
+
+    // detects all face rectangles from Mat image
+    public Rect[] detectFaces(Mat image) {
+        Mat gray = new Mat();
+        Mat mat = new Mat();
+        Imgproc.cvtColor(image, gray, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.equalizeHist(gray, mat);
+        if (getAbsoluteFaceSize() == 0) {
+            int height = mat.rows();
+            if (Math.round(height * getRelativeFaceSize()) > 0) {
+                setAbsoluteFaceSize(Math.round(height * relativeFaceSize));
+            }
+        }
+        MatOfRect faces = new MatOfRect();
+        if (getViolaJonesClassifier() != null)
+            getViolaJonesClassifier()
+                .detectMultiScale(mat, faces, getScaleFactor(),
+                    getMinNeighbors(), getFlags(), getMinSize(), new Size());
+        else
+            Log.e(TAG, "Viola-jones classifier is not loaded!");
+        return faces.toArray();
+    }
+
 
     // async loader of opencv resources
     private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
